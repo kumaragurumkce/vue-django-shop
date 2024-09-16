8d4eafc168b72f9cac1fcd82c2c7b36d9e421beb
kumaragurumkce <kumaragururf123@gmail.com>
2024-09-16 15:29:52 +0530

    deploy

--- todobackend/settings.py
+++ todobackend/settings.py
@@ -12,20 +12,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASE_URL="postgresql://postgres:ZaQeyHcLTrZvelFhXDmHRhKSpNlhTaBn@junction.proxy.rlwy.net:53266/railway"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iz6^ux_0b5_b@=yoc1l26bv=2ll9f+9b+iyeaitzu%x$jgh%oz'
-
+# SECRET_KEY = 'django-insecure-iz6^ux_0b5_b@=yoc1l26bv=2ll9f+9b+iyeaitzu%x$jgh%oz'
+SECRET_KEY=os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -79,16 +79,14 @@ WSGI_APPLICATION = 'todobackend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'shop1',
-    #     'USER':'postgres',
-    #     'PASSWORD':'1234',
-    #     'HOST':'localhost',
-    #     'PORT':'5432',
-    # }
-    "default":dj_database_url.config(default=DATABASE_URL,conn_max_age=1800),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR/ "db.sqlite3",
+    }
+    # "default":dj_database_url.config(default=DATABASE_URL,conn_max_age=1800),
 }
+database_url=os.environ.get("DATABASE_URL")
+DATABASES["default"]=dj_database_url.parse(database_url)
 
 
 # Password validation
