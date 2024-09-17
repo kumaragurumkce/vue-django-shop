5ced016754214e9f649f12645a9f2ecbbbdd6681
kumaragurumkce <kumaragururf123@gmail.com>
2024-09-17 13:18:09 +0530

    ji

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, redirect
+from django.db import DatabaseError
 from .models import Name
 
 def name_list(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         if name:
-            Name.objects.create(name=name)
+            try:
+                Name.objects.create(name=name)
+            except DatabaseError:
+                return render(request, 'myapp/name_list.html', {'error': 'Database error occurred while saving the name.'})
         return redirect('name_list')
 
-    names = Name.objects.all()
+    try:
+        names = Name.objects.all()
+    except DatabaseError:
+        return render(request, 'myapp/name_list.html', {'error': 'Database error occurred while fetching names.'})
+    
     return render(request, 'myapp/name_list.html', {'names': names})
