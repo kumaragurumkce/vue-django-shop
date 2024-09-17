c0f867abab4f51a7b5117239088029bf6f6a2151
kumaragurumkce <kumaragururf123@gmail.com>
2024-09-17 13:41:23 +0530

    h

--- render_deploy/settings.py
+++ render_deploy/settings.py
@@ -21,16 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-go-43ere!3z19tg57xp8w8a&p-=(+u_5u)$hv_w_pu!&gx55tc'
-SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+SECRET_KEY = 'django-insecure-go-43ere!3z19tg57xp8w8a&p-=(+u_5u)$hv_w_pu!&gx55tc'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+# DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS=[]
 
 # Application definition
 
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'render_deploy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'test1_x4ce',
         'USER':'test1_x4ce_user',
         'PASSWORD':'gZqEsPihU6E92Bu0VNpxu2Cc0XGmep66',
@@ -91,8 +91,9 @@ DATABASES = {
     }
     
 }
-database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
+
+# database_url = os.environ.get("DATABASE_URL")
+# DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
