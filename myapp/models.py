efa5e67eb735d518dbea6336953ec797ffc192cd
kumaragurumkce <kumaragururf123@gmail.com>
2024-09-17 13:04:13 +0530

    a

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Name(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
