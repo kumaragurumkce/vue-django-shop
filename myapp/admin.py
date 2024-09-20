feef1c326e66cc0795deb529af1d168debf75033
kumaragurumkce <kumaragururf123@gmail.com>
2024-09-20 15:50:00 +0530

    www

--- myapp/admin.py
+++
@@ -1,9 +0,0 @@
-# admin.py
-from django.contrib import admin
-from .models import Member
-
-class MemberAdmin(admin.ModelAdmin):
-    list_display = ('name',)  # This will display the 'name' field in the list view
-    search_fields = ('name',)  # This will allow searching by 'name'
-
-admin.site.register(Member, MemberAdmin)
