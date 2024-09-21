1e1c5823da954deb6c689c893ec3a27ff69187f5
kumaragurumkce <kumaragururf123@gmail.com>
2024-09-21 13:07:10 +0530

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# DRF_Tutorial
-Building a restful api for ecommerce web application, this is the source code for the 1st episode.
--- core/serializers.py
+++
@@ -1,7 +0,0 @@
-from djoser.serializers import UserCreateSerializer
-
-
-class MyUserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        fields = ["id", "email", "first_name", "last_name", "password", "username"]
-        
